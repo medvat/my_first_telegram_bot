5c6b41701abddb26fd73357342a567791e76752c
Sveta <sad@maw.ry>
2022-03-23 11:45:16 +0300

    error correction handler

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 
 bot = telebot.Telebot('5195333026:AAFyPOxPjzXMEHKbWaGYrMhtaFqh2l3oqmq')
 
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, '<b>Hello!<b>', parse_mode='html')
 
