86a87d12e1a95ab9e2ee7b7c74a55fad3476d48d
Sveta <sad@maw.ry>
2022-03-23 11:40:04 +0300

    create function for text output

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import telebot
 
-bot = telebot.Telebot('5195333026:AAFyPOxPjzXMEHKbWaGYrMhtaFqh2l3oqm')
+bot = telebot.Telebot('5195333026:AAFyPOxPjzXMEHKbWaGYrMhtaFqh2l3oqmq')
 
 @bot.message_handlers(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '<b>Hello!<b>', parse_mode='html')
+
+
+
