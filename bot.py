034e7730775cdc967aa5b3c850618e988a053ba4
Sveta <sad@maw.ry>
2022-03-23 11:51:25 +0300

    error correction </b>

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 import telebot
 
-bot = telebot.TeleBot('5195333026:AAFyPOxPjzXMEHKbWaGYrMhtaFqh2l3oqmq')
+bot = telebot.TeleBot('5195333026:AAFyPOxPjzXMEHKbWaGYrMhtaFqh2l3oqmg')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '<b>Hello!<b>', parse_mode='html')
+    bot.send_message(message.chat.id, '<b>Hello!</b>', parse_mode='html')
 
 bot.polling(none_stop=True)
 
