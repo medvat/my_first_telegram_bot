7cabc4f2d14127de281453c40e60913109fd4fa6
Sveta <sad@maw.ry>
2022-03-23 11:42:30 +0300

    add method polling for work the job

--- bot.py
+++ bot.py
@@ -6,5 +6,5 @@ bot = telebot.Telebot('5195333026:AAFyPOxPjzXMEHKbWaGYrMhtaFqh2l3oqmq')
 def start(message):
     bot.send_message(message.chat.id, '<b>Hello!<b>', parse_mode='html')
 
-
+bot.polling(none_stop=True)
 
